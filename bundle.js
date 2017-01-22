34cb682acf18d849c76c7381dfa2e77f425e6131
Luca Pellicoro <luca@hipmunk.com>
2017-01-21 19:05:16 -0800

    update bundle

--- bundle.js
+++ bundle.js
@@ -74,12 +74,13 @@
 
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-	var ELEMENT_HEIGHT = 40;
+	var ELEMENT_HEIGHT = 100;
+
 	var DragHandle = (0, _reactSortableHoc.SortableHandle)(function () {
 	    return _react2.default.createElement(
 	        'span',
-	        null,
-	        '==='
+	        { className: 'handle' },
+	        '\u2261'
 	    );
 	});
 
